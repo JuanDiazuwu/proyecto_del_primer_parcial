ba27ee46daafb2679d9675c5671262672e835b8d
Denzel Muñoz <a348802@uach.mx>
2023-03-13 12:50:20 -0600

    Agregando opciones de Agile con while

--- proyecto_bash/proyecto.sh
+++ proyecto_bash/proyecto.sh
@@ -7,7 +7,7 @@ if [ $1 == "-a" ]; then
 	while true; do 
 		echo ""
 		echo "Bienvenido a la guía rapida de Agile, para continuar seleccione un tema:" 
-		echo "1.SCRUM"
+		echo "1. SCRUM"
 		echo "2. X.P."
 		echo "3. Kanban"
 		echo "4. Crystal"
@@ -15,5 +15,35 @@ if [ $1 == "-a" ]; then
 		echo ""
 		read -p "Elija una opción : " selection_user
 		echo $selection_user
+		
+		case $selection_user in 
+			1 )
+				while true; do
+				       echo "Buenas"	
+				done
+			;;
+			2 )
+				while true; do
+				       echo "Buenas"	
+				done
+			;;
+			3 )
+				while true; do
+				       echo "Buenas"	
+				done
+			;;	
+			4 )
+				while true; do
+				       echo "Buenas"	
+				done
+			;;	
+			5 )
+				while true; do
+				       echo "Buenas"	
+				done
+			;;	
+		esac
 	done
-fi 
+fi
+
+

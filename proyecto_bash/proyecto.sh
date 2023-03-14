fd35749e6cbb4b07c274a9fc27763afbd214f1d7
JUAN ANTONIO DIAZ FERNANDEZ <a348637@uach.mx>
2023-03-14 10:56:33 -0600

    Agregando funciones de buscar, eliminar, y mostrar

--- proyecto_bash/proyecto.sh
+++ proyecto_bash/proyecto.sh
@@ -2,47 +2,82 @@
 
 function menu_two {
 	echo ""
-    	echo "Usted esta en la sección $1 seleccione la opción que desea utilizar."
-    	echo "1. Agregar información"
-    	echo "2. Buscar"
-   	echo "3. Eliminar información"
-    	echo "4. Leer base de información"
-    	echo "5. Salir al menú principal"
+	echo "Usted esta en la sección $1 seleccione la opción que desea utilizar."
+	echo "1. Agregar información"
+	echo "2. Buscar"
+	echo "3. Eliminar información"
+	echo "4. Leer base de información"
+	echo "5. Salir al menú principal"
    	echo ""
-   	read selection_menu_two
+   	read -p "Porfavor, seleccione una opción: " selection_menu_two
    	
-   	 case $selection_menu_two in
-        	1)
-            		save_info "$1"
-        	;;
-        	2)
-            		search "$1"
-        	;;
-        	3)
-            		delete "$1"
-        	;;
-        	4)
-            		read_uwu $1
-        	;;
-        	5)
-            		echo "Falta esta parte"
-        	;;
-        	*)
-            		echo "Porfavor escoja una respuesta correcta"
-        	;;
-    	esac
+   	case $selection_menu_two in
+		1)
+			save_info "$1"
+		;;
+		2)
+			search "$1"
+		;;
+		3)
+			delete "$1"
+		;;
+		4)
+			read_uwu $1
+		;;
+		5)
+			echo "Falta esta parte"
+		;;
+		*)
+			echo "Porfavor escoja una respuesta correcta"
+		;;
+	esac
 }
 
 function save_info {
-    	if [ ! -e "$1.info" ]; then
-        	touch "$1.info"
-        	echo "Archivo creado"
-    	fi
-    	read -p "Ingrese el concepto: " concept
-    	read -p "Ingrese la definición de $concept: " definition
-    	echo "[$concept] .- $definition." >> "$1.info"
+	if [ ! -e "$1.info" ]; then
+		touch "$1.info"
+		echo "Archivo creado"
+	fi
+	read -p "Ingrese el concepto: " concept
+	read -p "Ingrese la definición de $concept: " definition
+	echo "[$concept] .- $definition." >> "$1.info"
 }
 
+function search {
+	read -p "Ingrese la palabra que quiere buscar: " search_word
+	if grep -Eo '\[[^]]+\]' "$1.info" | grep "$search_word"; then 
+		echo "La palabra "$search_word" si existe"
+	else
+		echo "La palabra "$search_word" no existe"
+	fi
+}
+
+function _search {
+	if grep -Eo '\[[^]]+\]' "$1.info" | grep "$2"; then
+		return 0
+	else
+		return 1
+	fi
+}
+
+function delete {
+	read -p "Ingresa el concepto que deseas eliminar: " delete_word
+	_search $1 $delete_word
+	aux=$?
+	#echo "EL valor de aux: $aux"
+	if [ $aux == 0 ]; then 
+		sed -i "/\[.*$delete_word.*\]/d" "$1.info" 
+	else
+		echo "El concepto no existe en $1.info"
+	fi
+}
+
+function read_uwu {
+	echo ""
+	echo "El contenido del archivo $1.info es: "
+	echo ""
+	cat $1.info
+}
 
 if [ $# != 1 ]; then 
 	echo "Error de parámetros, se necesita uno: [-a | -t]"
@@ -115,13 +150,11 @@ elif [ $1 == "-t" ];then
 				done
 			;;	
 			4 )
-                		echo "Nos vemos!!! Gracias por utilizar nuestro programa <3"
-                		break
+				echo "Nos vemos!!! Gracias por utilizar nuestro programa <3"
+				break
 			;;
 		esac   
 	done
 else
 	echo "Escoge un parámetro válido: [-a | -t]"
 fi
-
-

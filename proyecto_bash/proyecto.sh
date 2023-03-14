bcfc249de644df84980608106a5e4d2829c80c79
JessyNunez <jessy.nunez0217@gmail.com>
2023-03-13 22:23:06 -0600

    Se agregó un menú base de metodologías tradicionales

--- proyecto_bash/proyecto.sh
+++ proyecto_bash/proyecto.sh
@@ -44,6 +44,40 @@ if [ $1 == "-a" ]; then
 			;;	
 		esac
 	done
+elif [ $1 == "-t" ];then
+	while true; do
+        	echo ""
+        	echo "Bienvenido a la guia rapida de metodologias tradicionales, para continuar seleccione un tema:"
+        	echo "1. Cascada"
+        	echo "2. Espiral"
+        	echo "3. Modelo V"
+        	echo "4. Terminar ejecución"
+        	read -p "Elija una opción: " selection_user
+        	case $selection_user in 
+			1 )
+				while true; do
+				       echo "Buenas"	
+				done
+			;;
+			2 )
+				while true; do
+				       echo "Buenas"	
+				done
+			;;
+			3 )
+				while true; do
+				       echo "Buenas"	
+				done
+			;;	
+			4 )
+				while true; do
+				       echo "Buenas"	
+				done
+			;;
+		esac   
+	done
+else
+	echo "Escoje un parametro valido: [-a | -t]"
 fi
 
 
